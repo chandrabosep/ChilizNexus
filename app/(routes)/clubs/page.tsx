4d03e0ffff2c6c750ec2f209ff9c35392dce701e
Chandra Bose <chandrabosep3112@gmail.com>
2024-08-25 00:29:40 +0530

    chore: revamp clubs

--- app/(routes)/clubs/page.tsx
+++ app/(routes)/clubs/page.tsx
@@ -3,14 +3,32 @@ import React from "react";
 
 const Clubs = () => {
 	return (
-		<div className="flex flex-col py-6 gap-y-10 w-full max-w-screen-2xl">
+		<div className="flex flex-col py-6 gap-y-10 w-full h-full max-w-screen-2xl">
 			<div className="flex items-center justify-between">
 				<h2 className={`text-3xl text-secondaryColor font-semibold`}>
 					Clubs
 				</h2>
 			</div>
-			<div className="flex flex-wrap items-center w-full gap-6">
-				<ClubCard />
+			<div className="flex flex-wrap items-center w-full gap-6 h-fit">
+				{[
+					{
+						title: "FC Barcelona",
+						img: "/fcb.jpeg",
+						desc: "Join the on-chain community of FC Barcelona fans, where the spirit of the Blaugrana lives on the blockchain. Connect with fellow supporters, participate in exclusive digital events, and engage in decentralized voting on club decisions. Whether it's celebrating victories or influencing future projects, this is the ultimate digital hub for true Barça enthusiasts",
+					},
+					{
+						title: "Paris Saint-Germain",
+						img: "/par.jpg",
+						desc: "Step into the world of Paris Saint-Germain on the blockchain. As a member of this on-chain club, you can participate in unique NFT drops, vote on club initiatives, and be part of a global network of PSG fans. From virtual meetups to fan-driven content, experience the magic of Les Parisiens in the digital era",
+					},
+					{
+						title: "Juventus F.C",
+						img: "/juv.jpg",
+						desc: "Welcome to the Juventus on-chain club, where Bianconeri fans converge in the digital realm. Take part in decentralized decision-making, access exclusive content, and showcase your passion with Juventus-themed NFTs. This is where tradition meets technology, allowing you to play an active role in the future of your beloved club",
+					},
+				].map((club, index) => (
+					<ClubCard key={index} data={club} />
+				))}
 			</div>
 		</div>
 	);
--- components/ClubCard.tsx
+++ components/ClubCard.tsx
@@ -2,11 +2,18 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { Card, CardContent } from "./ui/card";
-import { SquareArrowOutUpRight, Triangle } from "lucide-react";
+import {
+	Linkedin,
+	LinkIcon,
+	LucideLink,
+	SquareArrowOutUpRight,
+	Triangle,
+	Twitter,
+} from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-export default function ClubCard() {
+export default function ClubCard({ data }: any) {
 	const router = useRouter();
 
 	const [flipped, setFlipped] = useState(false);
@@ -26,12 +33,12 @@ export default function ClubCard() {
 
 	return (
 		<Card
-			className="w-full max-w-md h-fit min-h-[410px] min-w-[28rem]"
+			className="w-full max-w-md h-full min-h-[410px] min-w-[28rem]"
 			onClick={handleCardClick}
 		>
 			{!flipped && (
 				<Image
-					src={"/placeholder.jpg"}
+					src={data.img || "/placeholder.jpg"}
 					alt={"Project Image"}
 					width={400}
 					height={200}
@@ -45,30 +52,67 @@ export default function ClubCard() {
 			)}
 
 			<CardContent className="p-4 space-y-2 border-t h-full">
-				<div className="flex items-center justify-between">
-					<h3 className="text-xl font-bold">Hyderabad DAO</h3>
-					<Link href={`/explore/1`}>
-						<SquareArrowOutUpRight className="w-4 h-4 text-primaryColor" />
-					</Link>
-				</div>
 				{!flipped ? (
 					<>
-						<p className="text-muted-foreground line-clamp-3 leading-7 min-h-20">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Eos reprehenderit quibusdam dignissimos magni
-							facere nulla, iusto architecto corporis quo vitae
-							tempora nam natus exercitationem ut libero velit
-							commodi consequuntur tenetur.
-						</p>
-						<div className="flex items-center gap-2 text-sm text-muted-foreground">
-							<Triangle className="w-4 h-4 fill-primaryColor" />
-							<span>1.2K</span>
-							<Triangle className="w-4 h-4 fill-muted rotate-180" />
-							<span>200</span>
+						<div className="flex items-center justify-between">
+							<h3 className="text-xl font-bold">{data.title}</h3>
+							<Link href={`/explore/1`}>
+								<SquareArrowOutUpRight className="w-4 h-4 text-primaryColor" />
+							</Link>
 						</div>
+						<p className="text-muted-foreground line-clamp-4 leading-7 min-h-20">
+							{data.desc}
+						</p>
 					</>
 				) : (
-					<></>
+					<div className="flex flex-col justify-between h-full">
+						<div className="flex flex-col gap-y-2 h-fit">
+							<p className="font-semibold pt-1 px-2 bg-primaryColor text-white w-fit rounded">
+								ERC20
+							</p>
+							<h3 className="text-xl font-bold pt-4">
+								{data.title}
+							</h3>
+
+							<div className="grid grid-cols-2 items-center justify-between py-10">
+								<div className="mx-auto text-center">
+									<h6 className="text-xl font-bold">Fans</h6>
+									<p className="text-muted-foreground text-3xl">
+										10k+
+									</p>
+								</div>
+								<div className="mx-auto text-center">
+									<h6 className="text-xl font-bold">
+										Holders
+									</h6>
+									<p className="text-muted-foreground text-3xl">
+										26k+
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-col gap-y-2 h-fit">
+							<h6 className="text-xl font-bold">Socials</h6>
+							<div className="flex items-center gap-x-6 brightness-0">
+								<LucideLink
+									strokeWidth={3}
+									className="w-6 h-6"
+								/>
+								{["x.svg", "farcaster.svg", "discord.svg"].map(
+									(social) => (
+										<Image
+											key={social}
+											src={`/${social}`}
+											alt={social}
+											width={32}
+											height={32}
+											className="w-6 h-6"
+										/>
+									)
+								)}
+							</div>
+						</div>
+					</div>
 				)}
 			</CardContent>
 		</Card>
