53446c86de9c7b668a62a7821fb13d17a83d8237
Mujahid002 <mujahidshaik2002@gmail.com>
2024-08-26 17:17:44 +0530

    added chilizSpicy testnet for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,37 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.23",
+  networks: {
+    chilizSpicy: {
+      url: "https://spicy-rpc.chiliz.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: "mujahid002",
+    customChains: [
+      {
+        network: "chilizSpicy",
+        chainId: 88882,
+        urls: {
+          apiURL:
+            "https://api.routescan.io/v2/network/testnet/evm/88882/etherscan",
+          browserURL: "https://testnet.chiliscan.com",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: true,
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 10000,
+    },
+  },
 };
