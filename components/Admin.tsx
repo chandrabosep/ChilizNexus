71aae6fa4d689d35bf74e00e0df666a67979756e
Chandra Bose <chandrabosep3112@gmail.com>
2024-09-01 01:26:50 +0530

    chore update: admin

--- components/Admin.tsx
+++ components/Admin.tsx
@@ -25,6 +25,7 @@ import {
 } from "@/components/ui/table";
 import { client } from "@/lib/sanity";
 import { v4 as uuidv4 } from "uuid";
+import CreateUpdate from "./CreateUpdate";
 
 const formSchema = z.object({
 	name: z.string().min(1, { message: "Name is required" }),
@@ -97,95 +98,25 @@ export default function Admin({ data }: any) {
 	};
 	return (
 		<div className="flex flex-col gap-y-6 py-4">
-			<div>
-				<h1 className="text-2xl font-bold mb-4">Add New Entry</h1>
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(onSubmit)}
-						className="flex flex-col gap-y-6"
-					>
-						<div className="grid md:grid-cols-2 gap-x-10">
-							<div className="flex flex-col gap-y-6">
-								<FormField
-									control={form.control}
-									name="name"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Name</FormLabel>
-											<FormControl>
-												<Input
-													placeholder="Event Name"
-													{...field}
-												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="description"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Description</FormLabel>
-											<FormControl>
-												<Textarea
-													rows={6}
-													placeholder="What is the event about?"
-													{...field}
-												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="address"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Address</FormLabel>
-											<FormControl>
-												<Textarea
-													rows={6}
-													placeholder="Address of event"
-													{...field}
-												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-							</div>
-							<div className="flex flex-col gap-y-6">
-								<FormField
-									control={form.control}
-									name="date"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Date</FormLabel>
-											<FormControl>
-												<Input
-													type="date"
-													placeholder="Date of event"
-													{...field}
-													className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<div className="grid grid-cols-2 gap-x-4">
+			<div className="grid md:grid-cols-2 gap-x-10">
+				<div className="border-r-4 border-x-gray-200 pr-8">
+					<h1 className="text-2xl font-bold mb-4">Add New Entry</h1>
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(onSubmit)}
+							className="flex flex-col gap-y-6"
+						>
+							<div className="">
+								<div className="flex flex-col gap-y-6">
 									<FormField
 										control={form.control}
-										name="from"
+										name="name"
 										render={({ field }) => (
 											<FormItem>
-												<FormLabel>From</FormLabel>
+												<FormLabel>Name</FormLabel>
 												<FormControl>
 													<Input
-														placeholder="10:00 AM"
+														placeholder="Event Name"
 														{...field}
 													/>
 												</FormControl>
@@ -195,13 +126,33 @@ export default function Admin({ data }: any) {
 									/>
 									<FormField
 										control={form.control}
-										name="to"
+										name="description"
 										render={({ field }) => (
 											<FormItem>
-												<FormLabel>To</FormLabel>
+												<FormLabel>
+													Description
+												</FormLabel>
 												<FormControl>
-													<Input
-														placeholder="11:00 PM"
+													<Textarea
+														rows={6}
+														placeholder="What is the event about?"
+														{...field}
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name="address"
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Address</FormLabel>
+												<FormControl>
+													<Textarea
+														rows={6}
+														placeholder="Address of event"
 														{...field}
 													/>
 												</FormControl>
@@ -210,42 +161,101 @@ export default function Admin({ data }: any) {
 										)}
 									/>
 								</div>
+								<div className="flex flex-col gap-y-6">
+									<FormField
+										control={form.control}
+										name="date"
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Date</FormLabel>
+												<FormControl>
+													<Input
+														type="date"
+														placeholder="Date of event"
+														{...field}
+														className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<div className="grid grid-cols-2 gap-x-4">
+										<FormField
+											control={form.control}
+											name="from"
+											render={({ field }) => (
+												<FormItem>
+													<FormLabel>From</FormLabel>
+													<FormControl>
+														<Input
+															placeholder="10:00 AM"
+															{...field}
+														/>
+													</FormControl>
+													<FormMessage />
+												</FormItem>
+											)}
+										/>
+										<FormField
+											control={form.control}
+											name="to"
+											render={({ field }) => (
+												<FormItem>
+													<FormLabel>To</FormLabel>
+													<FormControl>
+														<Input
+															placeholder="11:00 PM"
+															{...field}
+														/>
+													</FormControl>
+													<FormMessage />
+												</FormItem>
+											)}
+										/>
+									</div>
 
-								<FormField
-									control={form.control}
-									name="price"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Price</FormLabel>
-											<FormControl>
-												<Input
-													type="number"
-													placeholder="Price"
-													{...field}
-													onChange={(e) =>
-														field.onChange(
-															Number(
-																e.target.value
+									<FormField
+										control={form.control}
+										name="price"
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Price</FormLabel>
+												<FormControl>
+													<Input
+														type="number"
+														placeholder="Price"
+														{...field}
+														onChange={(e) =>
+															field.onChange(
+																Number(
+																	e.target
+																		.value
+																)
 															)
-														)
-													}
-												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
+														}
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								</div>
 							</div>
-						</div>
 
-						<Button
-							type="submit"
-							className="w-full bg-primaryColor hover:bg-primaryColor/90"
-						>
-							Submit
-						</Button>
-					</form>
-				</Form>
+							<Button
+								type="submit"
+								className="w-full bg-primaryColor hover:bg-primaryColor/90"
+							>
+								Submit
+							</Button>
+						</form>
+					</Form>
+				</div>
+				<div>
+					<h2 className="text-xl font-bold mb-4">Updates</h2>
+					<CreateUpdate data={data} />
+				</div>
 			</div>
 			<div className="mt-8">
 				<h2 className="text-xl font-bold mb-4">Entries</h2>
