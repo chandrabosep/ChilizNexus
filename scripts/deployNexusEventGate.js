7ee93b7f882c3d54f69079d18f5ddb0a7da2b801
Mujahid002 <mujahidshaik2002@gmail.com>
2024-09-04 21:45:07 +0530

    deploy script for NexusEventGate

---
+++ scripts/deployNexusEventGate.js
@@ -0,0 +1,31 @@
+async function main() {
+  const nexusEventGateContract = await hre.ethers.getContractFactory(
+    "NexusEventGate"
+  );
+  console.log("Deploying NexusEventGate Contract...");
+
+  const NexusEventGateContract = await nexusEventGateContract.deploy({
+    gasLimit: 50000000,
+  });
+
+  await NexusEventGateContract.waitForDeployment();
+  const nexusEventGateAddress = await NexusEventGateContract.getAddress();
+  console.log("NexusEventGate Contract Address:", nexusEventGateAddress);
+  console.log("----------------------------------------------------------");
+
+  // Verify NexusEventGate Contract
+  console.log("Verifying NexusEventGate...");
+  await run("verify:verify", {
+    address: nexusEventGateAddress,
+    constructorArguments: [],
+  });
+  console.log("----------------------------------------------------------");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+// yarn hardhat run scripts/deployNexusEventGate.js --network chilizSpicy
+// yarn hardhat verify --network chilizSpicy DEPLOYED_CONTRACT_ADDRESS
